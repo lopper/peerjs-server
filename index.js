6d784bd0be7223b254a991baba83d4b957cb20cb
Patrick <patricklo@gmail.com>
2017-04-11 16:53:52 -0700

    add index file

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var ExpressPeerServer = require('./lib').ExpressPeerServer;
+
+
+var path = '/peerjs';
+var port = 9000;
+
+app.get('/', function(req, res, next) { res.send('Hello world!'); });
+
+var server = app.listen(port);
+
+var options = {
+    debug: false,
+	allow_discovery: false
+}
+
+// OR
+
+var server = require('http').createServer(app);
+
+
+app.use(path, ExpressPeerServer(server, options));
+
+
+server.listen(port);
+
+console.log(`Started PeerServer on ::, port: ${port}, path: ${path}`)
